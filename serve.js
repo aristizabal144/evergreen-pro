711bc65745955ce87f2b78e4f080af504667b32f
aristizabal144 <aristizabalcristian9005@gmail.com>
2021-09-24 20:21:58 -0500

    ok

--- serve.js
+++ serve.js
@@ -2,6 +2,14 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 3000;
 app.listen(port);
